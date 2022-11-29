499e7398024f263f88fca1137a268da4704c8918
PredragDjukic <predragdjukicit@gmail.com>
2022-11-29 08:50:20 +0100

    Update route and add response swagger annotation

--- src/jmbgValidator/validator.controller.ts
+++ src/jmbgValidator/validator.controller.ts
@@ -1,17 +1,18 @@
 import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 import { ValidatorService } from "./validator.service";
 import { JmbgDto } from "./dtos/jmbg.dto";
-import { ApiBody, ApiOperation } from "@nestjs/swagger";
+import { ApiBody, ApiCreatedResponse, ApiOperation } from "@nestjs/swagger";
 import { PersonDto } from "./dtos/person.dto";
 
 @Controller('validator')
 export class ValidatorController {
   constructor(private validatorService: ValidatorService) { }
 
-  @Post()
+  @Post('jmbg')
   @HttpCode(200)
   @ApiOperation({ summary: 'Validate' })
   @ApiBody({ type: JmbgDto, required: true })
+  @ApiCreatedResponse({type: PersonDto})
   validateJmbg(@Body() dto: JmbgDto): PersonDto {
     return this.validatorService.validate(dto);
   }
